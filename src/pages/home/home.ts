49845e8cc073c56a70abb56e3e01f1a7911f7551
williamoraes <william_rmoraes@hotmail.com>
2018-09-14 22:14:47 -0300

    alteração list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, List } from 'ionic-angular';
 import { NoticiasProvider } from '../../providers/noticias/noticias';
 
 @Component({
@@ -14,6 +14,7 @@ export class HomePage {
   links = new Array<any>();
   dateNew = [];
   content = new Array<any>();
+  news = [];
 
   constructor(public navCtrl: NavController, 
               private _noticiasProvider: NoticiasProvider) {
@@ -27,13 +28,14 @@ export class HomePage {
   getNews(){
     
    this._noticiasProvider.getNews()
+                        .toPromise()
                         .then(data =>{ 
-
+                          
                           var testeTitles = [];
                           var testeDates = [];
                           var testeLinks = [];
-                        
-                          var textResponse = (data.data as string);   
+                         
+                          var textResponse = (data as string);   
                           var _capTitle = textResponse.split("title");
                           var _capDate = textResponse.split("pubDate");
                           var _capLink = textResponse.split("link");
@@ -42,13 +44,24 @@ export class HomePage {
                           testeDates = this.treatsArray(_capDate);
                           testeLinks = this.treatsArray(_capLink);
 
+                           this.news = this.getListOfNews();
+                      //    console.log
+                        //  news[1].title = "will";
                           for(let c=1; c<testeTitles.length;c++){
-                             this.titles[c-1] = testeTitles[c].substring(1,testeTitles[c].length-2);
-                             this.dates[c-1] = testeDates[c-1].substring(1,testeDates[c-1].length-2);
-                             this.links[c-1] = testeLinks[c].substring(1,testeLinks[c].length-2);
+
+                            this.news[c-1].title = testeTitles[c].substring(1,testeTitles[c].length-2);
+                            this.news[c-1].date = testeDates[c-1].substring(1,testeDates[c-1].length-2);
+                            this.news[c-1].link = testeLinks[c].substring(1,testeLinks[c].length-2);
+                         //   this.teste.push(this.news[0]);
+                            this.titles[c-1] = testeTitles[c].substring(1,testeTitles[c].length-2);
+                        //     this.dates[c-1] = testeDates[c-1].substring(1,testeDates[c-1].length-2);
+                            this.links[c-1] = testeLinks[c].substring(1,testeLinks[c].length-2);
+                            // console.log(news);
                           }  
+ 
+                          console.log(this.news);
    
-                          this.formatDate(this.dates);
+                          this.formatDate(this.news);
                          
                           return this.links;   
 
@@ -78,25 +91,27 @@ export class HomePage {
     var _capImgEf = []
   
     for(let i=0; i< _data.length; i++){
-      this._noticiasProvider.getImage(_data[i]).then(data => { // requisição IMG
-        const responseImg = (data.data as string);
+      this._noticiasProvider.getImage(_data[i]).subscribe(data => { // requisição IMG
+
+        const responseImg = (data as string);
         _capImg = responseImg.split('"og:image" content="');
         _capImgEf = _capImg[1].split('" />');
-        this.content[i] = _capImgEf[0];
+        this.news[i].img = _capImgEf[0];
       });
     }
+    console.log(this.news)
   }
 
-  formatDate(date : Array<any>){
-    for(let i=0; i<date.length; i++){
-      this.dateNew[i] = date[i].substring(0, date[i].length - 5);
+  formatDate(array){
+    for(let i=0; i<array.length; i++){
+      this.dateNew[i] = array[i].date.substring(0, array[i].date.length - 5);
       var dataAtz = new Date(this.dateNew[i]);
         var dia=dataAtz.getDate();
         var mes=dataAtz.getMonth()+1;
         var ano=dataAtz.getFullYear();
         var hora=dataAtz.getHours();
         var minuto=dataAtz.getMinutes();
-        this.atzString[i]=this.zeroFill(dia,2)+"."+this.zeroFill(mes,2)+"."+ano+" | "+this.zeroFill(hora,2)+":"+this.zeroFill(minuto,2);
+        this.news[i].date=this.zeroFill(dia,2)+"."+this.zeroFill(mes,2)+"."+ano+" | "+this.zeroFill(hora,2)+":"+this.zeroFill(minuto,2);
     }
   }
 
@@ -110,4 +125,68 @@ export class HomePage {
   return number + ""; // always return a string
   }
 
+  getListOfNews(){
+    var ListOfNews = [{
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    },
+    {
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    },
+    {
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    },
+    {
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    },
+    {
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    },
+    {
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    },
+    {
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    },
+    {
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    },
+    {
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    },
+    {
+      title: "",
+      date: "",
+      link: "",
+      img: "",
+    }];
+
+    return ListOfNews;
+  }
 }
--- src/providers/noticias/noticias.ts
+++ src/providers/noticias/noticias.ts
@@ -1,23 +1,24 @@
 import { Injectable} from '@angular/core';
 import { HTTP } from '@ionic-native/http';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable()
 export class NoticiasProvider {
 
-  constructor(public http: HTTP) {
+  constructor(public http: HttpClient) {
   
   }
 
 
       getNews(){
           var urljson = 'https://www.oabpr.org.br/feed/';
-          return this.http.get(urljson, {}, {});
+          return this.http.get(urljson, {responseType: 'text'});
 
       }
 
       getImage(link){
-        return this.http.get(link, {responseType: 'text'}, {})
+        return this.http.get(link, {responseType: 'text'})
 
       }
 }
